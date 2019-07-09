e7aa2fb959e700867ca76ea4c05ee4a720f608bb
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-09 22:25:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,14 @@
 require 'open-uri'
+<<<<<<< HEAD
+=======
+require 'pry'
+>>>>>>> 3f8f1cddfce7af130e9b29c83674a5ae333650c5
 require "nokogiri"
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+<<<<<<< HEAD
     html = Nokogiri::HTML(open("#{index_url}")) 
        students_hash=[]
       
@@ -29,6 +34,16 @@ class Scraper
             student_prof_hash[:linkedin]= url if student.attribute("href").value.include?("linkedin")
             student_prof_hash[:github] = url if student.attribute("href").value.include?("github")
             student_prof_hash[:blog] = url if student.css("img").attribute("src").text.include?("rss")
+=======
+    #responsible for scraping the index page that lists all of the students 
+    Nokogiri::HTML(open("#{index_url}"))
+    
+  
+  end
+
+  def self.scrape_profile_page(profile_url)
+    #esponsible for scraping an individual student's profile page to get further information about that student
+>>>>>>> 3f8f1cddfce7af130e9b29c83674a5ae333650c5
   end
             student_prof_hash[:profile_quote] = html.css("div.profile-quote").text
             student_prof_hash[:bio] = html.css("div.bio-content p").text
